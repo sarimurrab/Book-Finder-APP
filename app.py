a68786369f6d944cd6e3062850c2ca78e168226e
Chaudhary Sarimurrab <samurrab@gmail.com>
2021-07-11 11:14:27 +0530

    Adding db config and migration

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # importing required modules
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask.helpers import url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 
 # initialize the Flask app
 app = Flask(__name__)
+
+# database configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'books.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "mysecret"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # application route
 @app.route('/')
